b88234ec1bec442079a7a48883508e3cc6fb506b
Shivaji_Singh <shivajisingh08071998@gmail.com>
2020-05-01 22:06:24 +0530

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log("Javascript file has been loaded");
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
