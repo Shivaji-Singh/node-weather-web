9536255080424aa346bec1649760da3dd03d0dcb
Shivaji_Singh <shivajisingh08071998@gmail.com>
2020-05-03 01:02:51 +0530

    Modified forecast text

--- src/util/foreCast.js
+++ src/util/foreCast.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const foreCast = (latitude, longitude, callback) => {
 
-    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=1161891e031ea3249a95d004393b4b37'
+    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=1161891e031ea3249a95d004393b4b37&units=metric'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -10,7 +10,7 @@ const foreCast = (latitude, longitude, callback) => {
         } else if ((typeof longitude === 'string') || (typeof latitude === 'string')) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${body.current.weather[0].description} is currently ${body.current.temp} degrees out. There is a ${body.current.uvi} chance of rain.`)
+            callback(undefined, `Mostly ${body.hourly[0].weather[0].main} throughout the day. It is currently ${body.current.temp} degrees out. The daily highest temperature is ${body.daily[0].temp.max} with a low of ${body.daily[0].temp.min}. There is ${body.current.humidity}% chance to humidity.`)
         }
     })
 
